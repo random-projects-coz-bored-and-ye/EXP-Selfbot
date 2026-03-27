dd4d376370a6e08a28afe99d13b231e6d81963cf
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2026-03-27 19:36:39 +1100

    Fully fixed?

--- main.py
+++ main.py
@@ -3,44 +3,11 @@ from discord import CustomActivity
 import asyncio
 import os
 import random
+import json
+import sys
 
-time1 = 120 # Will send msg every 2 minutes
+time1 = 20 # Will send msg every 20 seocnds
 presence = "Self bot | https://syntaxical.space" # Presence you want the account to have
-
-random_msgs = [
-    "Hey guys! I wish you luck for the future!",
-    "Good luck!",
-    "Hey guys! How are you!",
-    "How are you guys?",
-    "Hope everyone has a great day!",
-    "Wishing you all the best!",
-    "Have a wonderful week ahead!",
-    "What’s everyone up to?",
-    "How’s it going today?",
-    "Anyone watched something cool recently?",
-    "Keep pushing forward!",
-    "You all got this!",
-    "Stay positive, team!",
-    "Just dropping by to say hi!",
-    "Who’s up for a challenge?",
-    "Random thought: pizza is amazing.",
-    "Hi everyone!",
-    "Hello there!",
-    "Good vibes to all!",
-    "Hope you’re having a good one!",
-    "Sending positive energy your way!",
-    "Don’t forget to smile today!",
-    "Take care, everyone!",
-    "Keep grinding and shining!",
-    "Hey hey! What’s up?",
-    "Good morning, team!",
-    "Good afternoon, folks!",
-    "Good evening, friends!",
-    "Stay awesome, everyone!",
-    "ggs!",
-    "bazinga!"
-]
-
 
 ServerID = 1184110194905600132 # Server ID
 ChannelID = 1213975152145072128 # Channel ID
@@ -50,6 +17,19 @@ ChannelID = 1213975152145072128 # Channel ID
 token = "" # Your discord token
 
 # DONT EDIT UNLESS YOU KNOW WHAT YOUR DOING
+
+def GetRandomMsg(): # Makes a fucntion
+    try: # tries
+        with open("index.json", "r") as f: # Reads index.json and exports the data as 'f'
+            data = json.load(f) # Loads 'f'
+            msgs = data["Messages"] # get the msgs data
+            return random.choice(msgs) # Returns a random choice in the msgs array
+    except FileNotFoundError: # If the file isnt found it will print \/
+        print("FILE NOT FOUND | index.json")
+    except (KeyError, json.JSONDecodeError) as e: 
+        print(f"ERROR READING JSON: {e}")
+        sys.exit(1)
+
 
 class Myclient(discord.Client):
     async def on_ready(self): # WHen the bot is ready
@@ -64,7 +44,7 @@ class Myclient(discord.Client):
 
         await self.change_presence(activity=CustomActivity(name=presence, emoji="💫")) # Changes status to assert dominance
         while True: # Infinite loop
-            msg = random.choice(random_msgs)
+            msg = GetRandomMsg()
             await channel.send(msg) # Sends message to the selected channel
             print(f"Successfully sent! | MSG: {msg}") # Just making sure yk 
             await asyncio.sleep(time1) # Waits the selected tiem
@@ -72,6 +52,3 @@ class Myclient(discord.Client):
 
 client = Myclient() # Retrieves client (I think?)
 client.run(token)
-
-
-# Okay I think I fixed the issue where the msg gets deleted by adding like 30 msgs instead of 1
