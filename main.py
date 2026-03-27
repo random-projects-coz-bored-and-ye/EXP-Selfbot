755950b37c954dbba3a2ed3f55a26f986ba67217
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2026-03-27 19:12:48 +1100

    Implement random message selection for bot
    Replaced single message with a list of random messages to be sent periodically.

--- main.py
+++ main.py
@@ -2,10 +2,45 @@ import discord
 from discord import CustomActivity
 import asyncio
 import os
+import random
 
 time1 = 120 # Will send msg every 2 minutes
-msg = "Hey guys! I wish you luck for the future!" # msg to send every few minutes
 presence = "Self bot | https://syntaxical.space" # Presence you want the account to have
+
+random_msgs = [
+    "Hey guys! I wish you luck for the future!",
+    "Good luck!",
+    "Hey guys! How are you!",
+    "How are you guys?",
+    "Hope everyone has a great day!",
+    "Wishing you all the best!",
+    "Have a wonderful week ahead!",
+    "What’s everyone up to?",
+    "How’s it going today?",
+    "Anyone watched something cool recently?",
+    "Keep pushing forward!",
+    "You all got this!",
+    "Stay positive, team!",
+    "Just dropping by to say hi!",
+    "Who’s up for a challenge?",
+    "Random thought: pizza is amazing.",
+    "Hi everyone!",
+    "Hello there!",
+    "Good vibes to all!",
+    "Hope you’re having a good one!",
+    "Sending positive energy your way!",
+    "Don’t forget to smile today!",
+    "Take care, everyone!",
+    "Keep grinding and shining!",
+    "Hey hey! What’s up?",
+    "Good morning, team!",
+    "Good afternoon, folks!",
+    "Good evening, friends!",
+    "Stay awesome, everyone!",
+    "ggs!",
+    "bazinga!"
+]
+
 
 ServerID = 1184110194905600132 # Server ID
 ChannelID = 1213975152145072128 # Channel ID
@@ -23,23 +58,20 @@ class Myclient(discord.Client):
         print("EXP Self bot | https://syntaxical.space") # Prints advertisemnet
         print("\033[0m", end="") # tells terminal to stop styling
         print(" \033[1;31m[*]\033[0m Press \"CTRL + C\" to stop the script.")   # normal red   
-        print(f"This program will send the message {msg} every {time1/60} Minutes!")
+        print(f"This program will send the message a msg every {time1/60} Minutes!")
 
         channel = self.get_channel(ChannelID) # Retrieves channel
 
         await self.change_presence(activity=CustomActivity(name=presence, emoji="💫")) # Changes status to assert dominance
         while True: # Infinite loop
+            msg = random.choice(random_msgs)
             await channel.send(msg) # Sends message to the selected channel
-            print("Successfully sent!") # Just making sure yk 
+            print(f"Successfully sent! | MSG: {msg}") # Just making sure yk 
             await asyncio.sleep(time1) # Waits the selected tiem
 
 
 client = Myclient() # Retrieves client (I think?)
 client.run(token)
 
-# HOLY DUB, WORKS FIRST TRY, DUBBBBBBBBBBBBBBBB
-# NO AI, NO PRE-TESTING LETS FUCKING GOOOOOOOOOOOOOOOO
-# I did use refrence code though (https://github.com/transicle/chess-stats.py/blob/main/client.py)
 
-# Ez.gg
-# Gotta upd syntaxical.space now but its ight
+# Okay I think I fixed the issue where the msg gets deleted by adding like 30 msgs instead of 1
